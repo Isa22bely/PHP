eda8b92f145a3512c77a2632a501f69a53db6d11
Isa22bely <isabelyvga22@gmail.com>
2024-02-27 15:27:44 -0300

    27/02/24

---
+++ dados/d.php
@@ -0,0 +1,10 @@
+<?php
+$dados = array (
+    array('nome' => 'Maria', 'idade' => '60', 'estilomusical' => 'jazz'),
+    array('nome' => 'João', 'idade' => '34', 'estilomusical' => 'rock'),
+    array('nome' => 'Marta', 'idade' => '21', 'estilomusical' => 'eletronica'),
+    array('nome' => 'Fabio', 'idade' => '15', 'estilomusical' => 'pagode'),
+    array('nome' => 'Helena', 'idade' => '38', 'estilomusical' => 'rock'),
+    array('nome' => 'Fatima', 'idade' => '18', 'estilomusical' => 'rock')
+);
+?>
---
+++ pages/er.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Filtrar dados onde ”estilo” seja rock</title>
+</head>
+<body>
+    
+</body>
+</html>
