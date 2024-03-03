838f5e18939b5d8b6993583e3340eefe27c5e443
Isa22bely <isabelyvga22@gmail.com>
2024-03-03 14:37:53 -0300

    03/03

--- dados/d.php
+++ dados/d.php
@@ -1,10 +1,10 @@
 <?php
 $dados = array (
-    array('nome' => 'Maria', 'idade' => '60', 'estilomusical' => 'jazz'),
-    array('nome' => 'João', 'idade' => '34', 'estilomusical' => 'rock'),
-    array('nome' => 'Marta', 'idade' => '21', 'estilomusical' => 'eletronica'),
-    array('nome' => 'Fabio', 'idade' => '15', 'estilomusical' => 'pagode'),
-    array('nome' => 'Helena', 'idade' => '38', 'estilomusical' => 'rock'),
-    array('nome' => 'Fatima', 'idade' => '18', 'estilomusical' => 'rock')
+    array('nome' => 'Maria', 'idade' => '60', 'estilo' => 'jazz'),
+    array('nome' => 'João', 'idade' => '34', 'estilo' => 'rock'),
+    array('nome' => 'Marta', 'idade' => '21', 'estilo' => 'eletronica'),
+    array('nome' => 'Fabio', 'idade' => '15', 'estilo' => 'pagode'),
+    array('nome' => 'Helena', 'idade' => '38', 'estilo' => 'rock'),
+    array('nome' => 'Fatima', 'idade' => '18', 'estilo' => 'rock')
 );
 ?>
--- pages/tdc.php
+++ pages/tdc.php
@@ -1,11 +1,50 @@
+<?php
+ include 'd.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todos os dados cadastrados</title>
+    <link rel="stylesheet" href="/styles/principal.css">
 </head>
 <body>
+<header>
+        <h3>
+           Utilizando Arrays em PHP 
+        </h3>
+        <a href="/index.html">Inicio</a>
+    </header>
+    <div class="poster">
+        <h1>
+            Manipulação de Arrays em PHP
+        </h1>
+        <p>
+            Exibindo todos os dados cadastrados.
+        </p>
+    </div>
+    <div class="tabela">
+        <table>
+            <thead>
+                <tr>
+                <th scope="col">Nome</th>
+                <th scope="col">Idade</th>
+                <th scope="col">Estilo preferido</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($dados as $linha => $item):?>
+                    <tr>
+                        <th escope="row"><?php echo $item['nome'] ?></th>
+                        <th><?php echo $item['idade'] ?></th>
+                        <th><?php echo $item['estilo'] ?></th>
+                    </tr>
+                    <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
     
 </body>
 </html>
